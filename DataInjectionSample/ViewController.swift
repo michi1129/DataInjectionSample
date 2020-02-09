f611c16fed36fe04d6e18e068390a0bbac2cf129
ro <ro.michi1129@gmail.com>
2020-02-09 11:22:15 +0900

    create project

---
+++ DataInjectionSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataInjectionSample
+//
+//  Created by ro on 2020/02/09.
+//  Copyright © 2020 ro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
